0bebc225011b857cb081bf516da072c42a4fef9c
amirrhossein.ir <amirrhossein.ir@gmail.com>
2024-03-02 14:00:45 +0330

    update=>fix bug  blur backward menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,14 @@ const $ = document;
 const humbarger = $.querySelector(".humbarger");
 const menu = $.querySelector(".menu");
 const close = $.querySelector(".close");
-function clickHandler(flex, hidden) {
+const isblur = $.querySelectorAll(".isblur");
+function clickHandler(flex, hidden,blur) {
     return function() {
         menu.classList.add(flex);
         menu.classList.remove(hidden);
+        for(let i=0;i<isblur.length;i++)
+        isblur[i].classList.toggle(blur)
     }
 }
-humbarger.addEventListener("click", clickHandler("flex", "hidden"));
-close.addEventListener("click", clickHandler("hidden", "flex"));
+humbarger.addEventListener("click", clickHandler("flex", "hidden","blur"));
+close.addEventListener("click", clickHandler("hidden", "flex","blur"));
