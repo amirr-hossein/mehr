e87227e99548a40da97ad5526ce6261525bc0f19
amirrhossein.ir <amirrhossein.ir@gmail.com>
2024-03-05 16:54:59 +0330

    update=>section show shoes payattention

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,19 @@ module.exports = {
         Regular:["estedadRegular"],
         lightKalameh:["kalamehlight"],
         estedadBold:["estedadBold"],
+        estedadMediun:["estedadMedium"],
       },
       fontSize:{
         textIcon:"12px",
         itemMenu:"18px",
         titleHeroSection:"44px",
-        scroll:"16px"
+        scroll:"16px",
+        titleShowCard:"32px",
+        explainShowCard:"20px",
+        showMoreProduct:"14px"
+      },
+      boxShadow:{
+        card:"0px 0px 8px 0px rgba(13, 10, 44, 0.04)"
       }
     },
   },
